5c7b4275ec436ee9c750fe80e3f027626af4669c
Willam Alves Lima <w.alves.lima@gmail.com>
2020-04-05 23:29:19 +0000

    dashbord de usuario, alteracoes e mudanca de senha

--- helloWorld/accounts/urls.py
+++ helloWorld/accounts/urls.py
@@ -6,8 +6,11 @@ from helloWorld.accounts import views
 
 urlpatterns = [
     #path('entrar', v_auth.LoginView,{'template_name':'accounts/login.html'}, name = 'login' ),
+    path('', views.dashboard, name = 'dashboard' ),
     path('entrar', v_auth.LoginView.as_view(), name = 'login' ),
     path('sair', v_auth.LogoutView.as_view(), name = 'logout' ),
     path('cadastra-se', views.register, name = 'register' ),
+    path('editar', views.edit, name = 'edit' ),
+    path('editar-senha', views.edit_password, name = 'edit_password' )
 
 ]
