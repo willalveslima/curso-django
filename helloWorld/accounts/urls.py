3e293855c1ab538e38cdbdf4a919cd98fbb147be
Willam Alves Lima <w.alves.lima@gmail.com>
2020-04-06 22:12:28 +0000

    password reset

--- helloWorld/accounts/urls.py
+++ helloWorld/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import views as v_auth
-from django.urls import path
+from django.urls import path,re_path
 from helloWorld.accounts import views
 
 
@@ -10,6 +10,8 @@ urlpatterns = [
     path('entrar', v_auth.LoginView.as_view(), name = 'login' ),
     path('sair', v_auth.LogoutView.as_view(), name = 'logout' ),
     path('cadastra-se', views.register, name = 'register' ),
+    path('nova-senha', views.password_reset, name = 'password_reset' ),
+    re_path(r'^confirmar-nova-senha/(?P<key>\w+)/$', views.password_reset_confirm, name = 'password_reset_confirm' ),
     path('editar', views.edit, name = 'edit' ),
     path('editar-senha', views.edit_password, name = 'edit_password' )
 
--- helloWorld/courses/models.py
+++ helloWorld/courses/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 
-
-
-
 # Create your models here.
 
 
